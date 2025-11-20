87a385bffdf8f96513163807fbda80c34ca4cd3f
Ameen776 <metafecbook1@gmail.com>
2025-11-20 03:03:10 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,22 +1,14 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp, cert } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDYb722t6Oh4waMKW0AO1lRUbaXZJKuTC4",
-  authDomain: "bot-tlegram-9f4b5.firebaseapp.com",
-  databaseURL: "https://bot-tlegram-9f4b5-default-rtdb.firebaseio.com",
-  projectId: "bot-tlegram-9f4b5",
-  storageBucket: "bot-tlegram-9f4b5.firebasestorage.app",
-  messagingSenderId: "561534640067",
-  appId: "1:561534640067:web:4be5ed739278d0e2e66776",
-  measurementId: "G-K1T66T95S5"
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+initializeApp({
+  credential: cert(serviceAccount),
+});
+
+export const db = getFirestore();
