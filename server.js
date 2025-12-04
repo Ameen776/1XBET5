4b339f9d9ad50e9e57dc14a94f2850736fad30c0
Ameen776 <metafecbook1@gmail.com>
2025-12-04 13:13:36 +0300

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-app.get("/", (req, res) => res.send("Bot is Running..."));
-app.listen(process.env.PORT || 3000, () =>
-  console.log("Server Running...")
-);
-
-require("./bot.js");
